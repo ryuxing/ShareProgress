1f5c32eb72661a98487c90626806cdb847904ed5
ryuxing <31610107+ryuxing@users.noreply.github.com>
2022-06-13 13:35:14 +0900

    暫定完成

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
-import { getDatabase, ref, set, get, child, onDisconnect} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js"
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, setPersistence, browserSessionPersistence} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { getDatabase, ref, set, get, child, onDisconnect, onValue} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -21,7 +21,8 @@ async function GoogleAuth(){
     }
   }else{
     try{
-      let res = await signInWithPopup(auth,provider);
+      let res = await setPersistence(auth,browserSessionPersistence)
+        .then(()=>{return signInWithPopup(auth,provider)});
       console.log(res);
       let userInfo = res.user;
       return {
@@ -55,6 +56,27 @@ async function getSnapshot(path){
   }
   return value;
 }
+async function trackValue(
+  path,
+  callback = (s)=>{
+    return s.val();
+  },
+  once=false){
+  let value = {};
+  try{
+    callback(snapshot);
+  }catch(e){
+    console.error(e)
+    return {};
+  }
+}
+function myOnValue(path,callback){
+  onValue(ref(db,path),(snapshot)=>{
+    const rcv = snapshot.val();
+    callback(rcv);
+  });
+}
+
 async function setData(path,obj){
   set(ref(db,path),obj);
 }
@@ -62,6 +84,7 @@ function disconnect(path,obj){
   console.log(ref(db,path));
   onDisconnect(ref(db,path)).set(obj);
 }
+
 window.Firebase = {
   Auth:{
     auth: auth,
@@ -70,8 +93,10 @@ window.Firebase = {
   },
   RTDB:{
     get:getSnapshot,
+    track:trackValue,
     set:setData,
-    onDisconnect:disconnect
+    onDisconnect:disconnect,
+    onValue:myOnValue
     /*update:
   */}
 }
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 authInfo = null;
 NO_IMAGE = "";
+roomId="testroom"
 window.onload = async()=>{
     authInfo = Firebase.Auth.auth.currentUser;
     initAccountStatus("",true);
     document.getElementById("account").addEventListener("click",initAccountStatus);
     window.q = new ProgressQueue(1000);
-    document.querySelector("iframe").contentDocument.querySelector("#viewerContainer").addEventListener("scroll",(e)=>{
-        //console.warn({_scroll:e.target.scrollTop,_height:e.target. scrollHeight,ratio :Math.round((e.target.scrollTop/e.target.scrollHeight)*1000)/10});
-        window.q.set(Math.round((e.target.scrollTop/e.target.scrollHeight)*1000)/10);
+    document.querySelector("#viewer").src="./pdfjs/web/viewer.html?file=IPSJ-HCI09133009.pdf";
+    document.querySelector("#viewer").addEventListener("load",()=>{
+        document.querySelector("#viewer").contentDocument.querySelector("#viewerContainer").addEventListener("scroll",(e)=>{
+            window.q.set(Math.round((e.target.scrollTop/e.target.scrollHeight)*1000)/10);
+            window.q.name = document.querySelector("#viewer").contentWindow.PDFViewerApplication._docFilename;
+        });
     });
 }
 function onScrollPdfViewer(e){
@@ -41,7 +45,9 @@ async function initAccountStatus(elem, onlyDisplay){
 
         //RTDBで進捗共有
         q.start();
-        Firebase.RTDB.onDisconnect("progress/"+authInfo.uid,null);
+        new ProgressArea(authInfo.uid,{progress:0,file:""});
+        Firebase.RTDB.onDisconnect("progress/"+roomId+"/"+authInfo.uid,null);
+        Firebase.RTDB.onValue("progress/"+roomId,ProgressArea.receive);
     }else{
         await Firebase.Auth.signOut(Firebase.Auth.auth);
         document.querySelector("#account div").style.backgroundImage = `url("${NO_IMAGE}")`;
@@ -59,6 +65,7 @@ function joinRoom(){}
 
  class ProgressQueue{
     number;
+    name="";
     last_number=-1;
     interval
     interval_id=0;
@@ -68,7 +75,7 @@ function joinRoom(){}
     start(){
         this.interval_id = setInterval(this.do.bind(this),this.interval);
     }
-    set(num){
+    set(num,name="File"){
         this.number=num;
     }
     do(){
@@ -79,10 +86,135 @@ function joinRoom(){}
         }
         if(this.number == this.last_number){return;}
         this.last_number = this.number;
-        Firebase.RTDB.set("progress/"+authInfo.uid,this.number);
+        Firebase.RTDB.set("progress/"+roomId+"/"+authInfo.uid,{progress:this.number,file:this.name});
         console.log("Set.");
     }
     clear(){
         clearInterval(interval_id);
+    }
+}
+function getKeys(obj){
+    var list = [];
+    for (let key in obj){
+        list.push(key);
+    }
+    return list;
+}
+class ProgressArea{
+    static list={};
+    static receive(res){
+        var rcvKey = []
+        for (let key in res){
+            rcvKey.push(key);
+        }
+        var listkey = [];
+        for (let key in ProgressArea.list){
+            listkey.push(key);
+        }
+
+        console.log({"_/_/":"receive",res:res,rcvKey:rcvKey,listkey:listkey});
+        for(let rcv of rcvKey){
+            console.log({"_/_/":"loop",rcv:rcv,resrcv:res[rcv]});
+
+            if(listkey.indexOf(rcv)==-1){
+                new ProgressArea(rcv,res[rcv]);
+                listkey.push(rcv);
+            }
+            ProgressArea.list[rcv].update(rcv,res[rcv]);
+
+        }
+        console.log({"_/_/":"receive",res:res,rcvKey:rcvKey,listkey:listkey});
+        let removekey = [];
+        for(let l of listkey){
+            if(rcvKey.indexOf(l)==-1){
+                removekey.push(l);
+            }
+        }
+        console.log({rmvkey:removekey});
+        for(let rmv of removekey){
+            ProgressArea.list[rmv].remove(rmv);
+        }
+    }
+    elem;
+    constructor(uid,obj){
+        console.log({"_/_/":"constructor",uid:uid,obj:obj});
+        var wrapper = document.createElement("div");
+        wrapper.classList.add("member");
+        wrapper.classList.add(uid);
+        document.querySelector(".progressWrapper").appendChild(wrapper);
+        //svg
+        var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+        svg.setAttribute("version","1.1");
+        svg.setAttribute("width","40");
+        svg.setAttribute("height","40");
+        svg.classList.add("icon");
+        wrapper.appendChild(svg);
+        //.member svg circle
+        var circle = document.createElementNS("http://www.w3.org/2000/svg","circle");
+        circle.classList.add("ratio");
+        circle.setAttribute("width",40);
+        circle.setAttribute("height",40);
+        circle.setAttribute("r","17.5");
+        circle.setAttribute("transform","rotate(-90 20 20)");
+        circle.setAttribute("stroke-dashoffset",""+(100 - obj.progress)*1.1);
+        svg.appendChild(circle);
+        //.member svg image
+        var svgimg = document.createElementNS("http://www.w3.org/2000/svg","image");
+        svgimg.setAttribute("x","5");
+        svgimg.setAttribute("y","5");
+        svgimg.setAttribute("width","30");
+        svgimg.setAttribute("height","30");
+        svgimg.setAttribute("clip-path","url(#round-icon)");
+        svgimg.setAttribute("href","");//URL指定
+        svg.appendChild(svgimg);
+        //.member .option
+        var opt  = document.createElement("div");
+        opt.classList.add("option");
+        wrapper.appendChild(opt);
+        //.member .option .progress
+        var progressEle  = document.createElement("span");
+        progressEle.classList.add("progress");
+        progressEle.innerText=obj.progress+"%";
+        opt.appendChild(progressEle);
+        //.member .option .doc
+        var doc =  document.createElement("span");
+        doc.classList.add("doc");
+        doc.innerText = obj.file;
+        opt.appendChild(doc);
+        //.member .option .name
+        var name =  document.createElement("span");
+        name.classList.add("name");
+        name.innerText = "";
+        opt.appendChild(name);
+
+        //後処理
+        this.elem = wrapper;
+        ProgressArea.list[uid] = this;
+        this.getProfile(uid);
+
+    }
+    async getProfile(uid){
+        var info = await Firebase.RTDB.get("profile/"+uid);
+        console.log(ProgressArea.list);
+        var elem = ProgressArea.list[uid].elem;
+        //写真
+        var svgimg = elem.querySelector("image");
+        svgimg.setAttribute("href",info.photoURL);//URL指定
+        //名前
+        elem.querySelector(".name").innerText = info.name;
+    }
+    remove(uid){
+        ProgressArea.list[uid].elem.remove();
+        delete ProgressArea.list[uid];
+    }
+    update(uid,obj){
+        console.log(obj);
+        var elem = document.querySelector(".member."+uid);
+        var svgprog = elem.querySelector("svg circle");
+        var txtprog = elem.querySelector(".option .progress");
+        var txtdoc = elem.querySelector(".option .doc");        
+        svgprog.setAttribute("stroke-dashoffset",(101-obj.progress)*1.1);
+        txtprog.innerText= obj.progress + "%";
+        txtdoc.innerText = obj.file;
     }
 }
